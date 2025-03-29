5b7083e5b63309f2d211a3718349d17f67431118
keshav goel <keshavgoel159@gmail.com>
2025-03-29 08:27:59 +0530

    complete flow with testNg Assertions

--- src/test/java/Basics/GoogleMapAPis.java
+++ src/test/java/Basics/GoogleMapAPis.java
@@ -1,8 +1,14 @@
 package Basics;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
+import org.testng.Assert;
+
+import files.Payloads;
 
 public class GoogleMapAPis {
 
@@ -17,29 +23,71 @@ public class GoogleMapAPis {
 		not necessarily required for other validation like header*/
 		//log().all() to log everything in the output as a part of request and response
 		//the same methods used in given() is when used in then () acts as output validation
+		// If you want to extract the response and then perform any validation then you need to parse the JSON
+		//JsonPath Class takes string as Input and convert that to JSON 
+		//if complex json then we need to use parent.child[index].child
+		// 2 ways to do assertion one directly using body() and hamcrest package or you first extract and convert to json and then validate
+		//passing dynamic values you need --->>  "+variable+"
 		
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		String resp =given().
+		
+		log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body(Payloads.addPlace())
+		.when().
+		
+		post("/maps/api/place/add/json")
+		.then().
+		
+		log().all().assertThat().statusCode(200).body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().asPrettyString();
+		System.out.println("extract As String ="+resp);
+		JsonPath js =new JsonPath(resp);
+		String placeid = js.getString("place_id");
+		System.out.println(placeid);
+		System.out.println("***********************************************");
+		
+		//Add place -->> update place -->> getPlace to check whether successfully added or not so for that need to store place Id 
+		String updtAdress ="80 winter walk, USA";
+		
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"keshav house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}\r\n"
-				+ "")
-		.when().post("/maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-		.header("Server", "Apache/2.4.52 (Ubuntu)");
+				+ "\"place_id\":\""+placeid+"\",\r\n"
+				+ "\"address\":\""+updtAdress+"\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("/maps/api/place/update/json")
+		.then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		System.out.println("***********************************************");
+		String getPlaceresponse = given().log().all().queryParam("key","qaclick123").queryParam("place_id", placeid)
+		.when().get("/maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200).extract().response().asString();
+		
+		JsonPath js1 = new JsonPath(getPlaceresponse);
+		String updatedAdress = js1.getString("address");
+		Assert.assertEquals(updtAdress, updatedAdress);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 	
 	
---
+++ src/test/java/files/Payloads.java
@@ -0,0 +1,28 @@
+package files;
+
+public class Payloads {
+	//return in String since it will be passed within body and that requires Payload in string format
+	//making the method static so that directly use without creating object
+	
+	public static String addPlace()
+	{
+		return "{\r\n"
+				+ "  \"location\": {\r\n"
+				+ "    \"lat\": -38.383494,\r\n"
+				+ "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n"
+				+ "  \"accuracy\": 50,\r\n"
+				+ "  \"name\": \"keshavs house\",\r\n"
+				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
+				+ "  \"types\": [\r\n"
+				+ "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n"
+				+ "  ],\r\n"
+				+ "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n"
+				+ "}\r\n"
+				+ "";
+	}
+
+}
